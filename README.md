55d96e33e2c3ff8abc632bd9e2c32c1b03dd0667
Udzu <uri.granta+python@gmail.com>
2019-12-11 13:21:20 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# pudzu
+
+## Summary
+
+Various Python 3.6+ packages, mostly geared towards dataviz and used to create the data visualisations in this [flickr account](https://www.flickr.com/photos/zarfo/albums) (most of which been posted to reddit at some point by [/u/Udzu](https://www.reddit.com/user/Udzu/)). The packages aren't properly tested yet but are reasonably simple, generic and docstringed.
+
+For sample scripts that use these packages, see the [pudzu](https://github.com/Udzu/pudzu) repo.
+
+## Installation
+
+The packages can be installed individually using `pip install [package-name]` (for the latest release) or by directly running `python setup.py install` (which installs all of them).
+
+## Packages
+
+- [pudzu-utils](docs/utils.md) - various utility functions and data structures.
+- [pudzu-dates](docs/dates.md) - date classes supporting flexible calendars, deltas and ranges.
+- [pudzu-pillar](docs/pillar.md) - various monkey-patched Pillow utilities.
+- [pudzu-charts](docs/charts.md) - Pillow-based charting tools.
+
+## Copyright
+
+Copyright © 2017–19 by Udzu. Licensed under the [MIT License](LICENSE).
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+VERSION='0.9.1'
+URL='https://github.com/Udzu/pudzu-packages/'
+AUTHOR='Uri Granta'
+AUTHOR_EMAIL='uri.granta+python@gmail.com'
+LICENSE='MIT'
+CLASSIFIERS=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+]
+PYTHON_REQUIRES='~=3.6'
+
+setup(name='pudzu-utils', version=VERSION, py_modules=['pudzu.utils'],
+      description='Various utility functions and data structures used by the other pudzu packages.',
+      keywords='pudzu utilities',
+      url=URL, author=AUTHOR, author_email=AUTHOR_EMAIL, license=LICENSE, classifiers=CLASSIFIERS,
+      install_requires=['toolz'], python_requires=PYTHON_REQUIRES)
+
+setup(name='pudzu-dates', version=VERSION, py_modules=['pudzu.dates'],
+      description='Date classes supporting flexible calendars, date deltas and ranges.',
+      keywords='pudzu date calendar',
+      url=URL, author=AUTHOR, author_email=AUTHOR_EMAIL, license=LICENSE, classifiers=CLASSIFIERS,
+      install_requires=['pudzu-utils'], python_requires=PYTHON_REQUIRES)
+
+setup(name='pudzu-pillar', version=VERSION, py_modules=['pudzu.pillar'],
+      description='Various imaging utilities, monkey-patched onto Pillow.',
+      keywords='pudzu imaging pillow',
+      url=URL, author=AUTHOR, author_email=AUTHOR_EMAIL, license=LICENSE, classifiers=CLASSIFIERS,
+      install_requires=['pudzu-utils', 'Pillow', 'numpy'], python_requires=PYTHON_REQUIRES)
+
+setup(name='pudzu-charts', version=VERSION, py_modules=['pudzu.charts'],
+      description='Pillow-based charting tools.',
+      keywords='pudzu charts pillow',
+      url=URL, author=AUTHOR, author_email=AUTHOR_EMAIL, license=LICENSE, classifiers=CLASSIFIERS,
+      install_requires=['pudzu-pillar', 'pudzu-dates', 'pandas'], python_requires=PYTHON_REQUIRES)
