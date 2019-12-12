50a0a39fa8f35803bc2806b96bd5a5237aa3bf04
Udzu <uri.granta+python@gmail.com>
2019-12-12 11:42:02 +0000

    Fix setup.py to include pudzu package

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
 VERSION='0.9.1'
+PACKAGES=['pudzu']
 URL='https://github.com/Udzu/pudzu-packages/'
 AUTHOR='Uri Granta'
 AUTHOR_EMAIL='uri.granta+python@gmail.com'
@@ -12,25 +13,25 @@ CLASSIFIERS=[
 ]
 PYTHON_REQUIRES='~=3.6'
 
-setup(name='pudzu-utils', version=VERSION, py_modules=['pudzu.utils'],
+setup(name='pudzu-utils', version=VERSION, packages=PACKAGES, py_modules=['pudzu.utils'],
       description='Various utility functions and data structures used by the other pudzu packages.',
       keywords='pudzu utilities',
       url=URL, author=AUTHOR, author_email=AUTHOR_EMAIL, license=LICENSE, classifiers=CLASSIFIERS,
       install_requires=['toolz'], python_requires=PYTHON_REQUIRES)
 
-setup(name='pudzu-dates', version=VERSION, py_modules=['pudzu.dates'],
+setup(name='pudzu-dates', version=VERSION, packages=PACKAGES, py_modules=['pudzu.dates'],
       description='Date classes supporting flexible calendars, date deltas and ranges.',
       keywords='pudzu date calendar',
       url=URL, author=AUTHOR, author_email=AUTHOR_EMAIL, license=LICENSE, classifiers=CLASSIFIERS,
       install_requires=['pudzu-utils'], python_requires=PYTHON_REQUIRES)
 
-setup(name='pudzu-pillar', version=VERSION, py_modules=['pudzu.pillar'],
+setup(name='pudzu-pillar', version=VERSION, packages=PACKAGES, py_modules=['pudzu.pillar'],
       description='Various imaging utilities, monkey-patched onto Pillow.',
       keywords='pudzu imaging pillow',
       url=URL, author=AUTHOR, author_email=AUTHOR_EMAIL, license=LICENSE, classifiers=CLASSIFIERS,
       install_requires=['pudzu-utils', 'Pillow', 'numpy'], python_requires=PYTHON_REQUIRES)
 
-setup(name='pudzu-charts', version=VERSION, py_modules=['pudzu.charts'],
+setup(name='pudzu-charts', version=VERSION, packages=PACKAGES, py_modules=['pudzu.charts'],
       description='Pillow-based charting tools.',
       keywords='pudzu charts pillow',
       url=URL, author=AUTHOR, author_email=AUTHOR_EMAIL, license=LICENSE, classifiers=CLASSIFIERS,
